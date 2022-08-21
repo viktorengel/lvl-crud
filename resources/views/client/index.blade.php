374d11f17afd08c46a88ca0f375e7da0fc033060
Viktor Rengel <viktorengel@hotmail.com>
2022-08-21 10:48:58 -0500

    capitulo 10

---
+++ resources/views/client/index.blade.php
@@ -0,0 +1,21 @@
+@extends('theme.base')
+
+@section('content')
+    <div class="container py-5 text-center">
+        <h1>Listado de clientes</h1>
+        <a href="{{ route('client.index') }}" class="btn btn-primary">Crear clientes</a>
+
+        <table class="table">
+            <thead>
+                <th>Nombre</th>
+                <th>Saldo</th>
+                <th>Acciones</th>
+            </thead>
+            <tbody>
+                <td>FelinoHost</td>
+                <td>0.0</td>
+                <td>Editar - Eliminar</td>
+            </tbody>
+        </table>
+        
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,8 @@
+@extends('theme.base')
+
+@section('content')
+    <div class="container py-5 text-center">
+        <h1>Hola Mundo</h1>
+        <a href="{{ route('client.index') }}" class="btn btn-primary">Clientes</a>
+    </div>
+@endsection
